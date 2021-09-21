28b230dbcdce3ceeb676c5dcdd8800bb78c2593c
MrArthurEmanuel <91098280+MrArthurEmanuel@users.noreply.github.com>
2021-09-20 21:14:49 -0300

    Add files via upload

--- pingmultiplo.py
+++ pingmultiplo.py
@@ -1,13 +1,20 @@
 import os
 import time
+print('#' *41)
+print('### Testador de IP''s e Hosts MULTIPLOS ###')
+print('#' *41)
+print('\nBy. Mr.Arthur (EU MESMO)')
 
 with open('hosts.txt') as file:
     dump = file.read()
     dump = dump.splitlines()
 
     for ip in dump:
-        print('Verificando o IP: ', ip )
+        print('\nVerificando o IP: ', ip )
         print('-' *60)
         os.system('ping -n 2 ' + ip)
         print('-' * 60)
         time.sleep(5)
+
+time.sleep(2)
+os.system('pause')
